7edf410a4c9cb8d439aee9b59c2f1eb971935caf
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-23 17:29:55 -0400

    created plays data file

---
+++ src/choices.js
@@ -0,0 +1,17 @@
+export const plays = [
+     {
+          rock: {
+               name: "ROCK",
+          }
+     },
+     {
+          paper: {
+               name: "PAPER",
+          }
+     },
+     {
+          scissors: {
+               name: "SCISSORS",
+          }
+     },
+]
