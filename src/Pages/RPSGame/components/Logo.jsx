627a1f2b56da7af00170d23109ce915a34915a2d
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-26 08:47:51 -0400

    created logo component

---
+++ src/Pages/RPSGame/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+     return (
+          <div className="logo-img-container">
+               <img src="/images/rock-paper-scissors.png" alt=""/>
+          </div>
+     )
+}
+
+export default Logo
