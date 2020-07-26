f1c5f22a531db843b6849ea2c04753c5abdbf03f
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-26 08:46:46 -0400

    added class wrapper to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Logo from './Pages/RPSGame/components/Logo';
+import ScoreCard from './Pages/RPSGame/components/ScoreCard';
 import RockPaperScissorsContainer from './Pages/RPSGame/containers/RockPaperScissorsContainer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App wrapper">
+      <header>
+        <div className="logo"><Logo /></div>
+        <div className="score-card"><ScoreCard /></div>
+      </header>
       <RockPaperScissorsContainer />
     </div>
   );
